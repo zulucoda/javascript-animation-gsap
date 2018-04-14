a80596c1f40f14376cc1e87c020b4d750e97fd25
Muzi (zuluCoda) <muzikayise.buthelezi@liberty.co.za>
2018-04-14 06:58:11 +0400

    [muzi] refactoring onComplete to pass params in :fist:

--- module-pure-js-1/module-pure-js-1.js
+++ module-pure-js-1/module-pure-js-1.js
@@ -4,16 +4,17 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function startTween() {
-    const toolBoxEl = document.getElementById('toolBox');
-    TweenLite.to(toolBoxEl, 1, {x: 290, y: 225, scale: 1.5, onComplete: returnToNormal });
+    const toolBoxEl = '#toolBox';
+    TweenLite.to(toolBoxEl, 1, {x: 290, y: 225, scale: 1.5, onComplete: returnToNormal, onCompleteParams: ['#toolBox'] });
 
     const greenWord = document.getElementsByClassName('asapReg');
     TweenLite.to(greenWord, 1, {color: '#ff0000', fontSize: '+=65', onComplete: returnTextToNormal});
 }
 
-function returnToNormal() {
-    const toolBoxEl = document.getElementById('toolBox');
-    TweenLite.to(toolBoxEl, 1, {x:0, y:0, scale: 1});
+function returnToNormal(elementLabel) {
+    if (elementLabel) {
+        TweenLite.to(elementLabel, 1, {x:0, y:0, scale: 1});
+    }
 }
 
 function returnTextToNormal() {
