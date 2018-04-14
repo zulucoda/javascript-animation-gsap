b8d139c55a5bc69e06f2eca1ac24e25b38c0a034
Muzi (zuluCoda) <muzikayise.buthelezi@liberty.co.za>
2018-04-14 06:34:15 +0400

    [muzi] update animation to use a better way to increase font size :fist:

--- module-pure-js-1/module-pure-js-1.js
+++ module-pure-js-1/module-pure-js-1.js
@@ -8,5 +8,5 @@ function startTween() {
     TweenLite.to(toolBoxEl, 1, {x: 290, y: 225, scale: 1.5});
 
     const greenWord = document.getElementsByClassName('asapReg');
-    TweenLite.to(greenWord, 1, {color: '#ff0000', fontSize: '150%'});
+    TweenLite.to(greenWord, 1, {color: '#ff0000', fontSize: '+=65'});
 }
