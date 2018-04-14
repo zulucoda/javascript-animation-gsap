1ae13ebc37e6fd4cf2a58de6dfd243f6f1173f6c
Muzi (zuluCoda) <muzikayise.buthelezi@liberty.co.za>
2018-04-14 06:51:47 +0400

    [muzi] adding onComplete callback for greenText to return to normal after animation :fist:

--- module-pure-js-1/module-pure-js-1.js
+++ module-pure-js-1/module-pure-js-1.js
@@ -8,10 +8,14 @@ function startTween() {
     TweenLite.to(toolBoxEl, 1, {x: 290, y: 225, scale: 1.5, onComplete: returnToNormal });
 
     const greenWord = document.getElementsByClassName('asapReg');
-    TweenLite.to(greenWord, 1, {color: '#ff0000', fontSize: '+=65'});
+    TweenLite.to(greenWord, 1, {color: '#ff0000', fontSize: '+=65', onComplete: returnTextToNormal});
 }
 
 function returnToNormal() {
     const toolBoxEl = document.getElementById('toolBox');
     TweenLite.to(toolBoxEl, 1, {x:0, y:0, scale: 1});
+}
+
+function returnTextToNormal() {
+    TweenLite.to('.asapReg', 1, {color: '#fff', fontSize: '-=65'});
 }
