f11202283ad9a74eb5994112611699f27afa9bd9
Muzi (zuluCoda) <muzikayise.buthelezi@liberty.co.za>
2018-04-14 06:42:35 +0400

    [muzi] adding onComplete callback function which will get called after animation is complete :fist:

--- module-pure-js-1/module-pure-js-1.js
+++ module-pure-js-1/module-pure-js-1.js
@@ -5,8 +5,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function startTween() {
     const toolBoxEl = document.getElementById('toolBox');
-    TweenLite.to(toolBoxEl, 1, {x: 290, y: 225, scale: 1.5});
+    TweenLite.to(toolBoxEl, 1, {x: 290, y: 225, scale: 1.5, onComplete: returnToNormal });
 
     const greenWord = document.getElementsByClassName('asapReg');
     TweenLite.to(greenWord, 1, {color: '#ff0000', fontSize: '+=65'});
+}
+
+function returnToNormal() {
+    const toolBoxEl = document.getElementById('toolBox');
+    TweenLite.to(toolBoxEl, 1, {x:0, y:0, scale: 1});
 }
