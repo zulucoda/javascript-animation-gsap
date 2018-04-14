1dd5e86dde01aa9db70763d6291151db89ba66b6
Muzi (zuluCoda) <muzikayise.buthelezi@liberty.co.za>
2018-04-14 16:28:25 +0400

    [muzi] adding timeline animation :fist:

---
+++ timeline-pure-js/timeline-pure-js.js
@@ -0,0 +1,26 @@
+// DOME Ready
+document.addEventListener('DOMContentLoaded', function () {
+    startTween()
+});
+
+function startTween() {
+    const delay = 1;
+
+    const toolBoxEl = '#toolBox';
+    TweenLite.to(toolBoxEl, 1, {delay: delay, x: 290, y: 225, scale: 1.5, onComplete: returnToNormal,
+        onCompleteParams: ['#toolBox'] });
+
+    const greenWord = document.getElementsByClassName('asapReg');
+    TweenLite.to(greenWord, 1, {delay: (delay + 0.5), color: '#ff0000', fontSize: '+=65',
+        onComplete: returnTextToNormal, onCompleteParams: [delay]});
+}
+
+function returnToNormal(elementLabel) {
+    if (elementLabel) {
+        TweenLite.to(elementLabel, 1, {x:0, y:0, scale: 1});
+    }
+}
+
+function returnTextToNormal(delay) {
+    TweenLite.to('.asapReg', 1, {delay: delay,color: '#fff', fontSize: '-=65'});
+}
